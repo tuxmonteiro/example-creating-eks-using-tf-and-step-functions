d4f76efd54244964bfaf55336dc22a9273de3362
Marcelo Teixeira Monteiro <marcelo.monteiro@olxbr.com>
2022-12-07 21:57:04 -0300

    remove unecessary output

--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
 output "state_machine_arn" {
   value = aws_sfn_state_machine.eks_cluster_management_state_machine.id
 }
-
-output "execution_input" {
-  description = "Sample input to StartExecution."
-  value       = "{}"
-}
